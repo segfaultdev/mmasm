79e885a97e03d5a6dd82f27bcaf1dc66886963c1
segfaultdev <segfaultdev@protonmail.com>
2022-08-13 01:18:51 +0200

    added all the required structs

--- include/mmasm.h
+++ include/mmasm.h
@@ -1,14 +1,81 @@
 #ifndef __MMASM_H__
 #define __MMASH_H__
 
+#include <stdint.h>
+
 typedef struct mm_word_t mm_word_t;
+typedef struct mm_input_t mm_input_t;
+
+typedef struct mm_label_t mm_label_t;
+typedef struct mm_empty_t mm_empty_t;
+typedef struct mm_output_t mm_output_t;
 
 enum {
-  mm_word_
+  mm_word_null,
+  mm_word_eof,
+  
+  mm_word_func,
+  mm_word_end,
+  
+  mm_word_colon,
+  mm_word_left_paren,
+  mm_word_right_paren,
+  mm_word_left_bracket,
+  mm_word_right_bracket,
+  
+  mm_word_plus,
+  mm_word_minus,
+  
+  mm_word_register,
+  mm_word_number,
+  mm_word_name,
 };
 
 struct mm_word_t {
+  int type;
   
+  union {
+    int register_value;
+    int32_t number_value;
+    char name_value[32];
+  };
 };
+
+struct mm_input_t {
+  mm_word_t *words;
+  int count, offset;
+};
+
+struct mm_label_t {
+  char name[32];
+  uint16_t addr;
+};
+
+struct mm_empty_t {
+  int offset, size; // offset in tokens, size can be either 1 or 2
+  uint16_t addr;
+};
+
+struct mm_output_t {
+  uint8_t *data;
+  int size;
+  
+  mm_label_t *labels;
+  int label_count;
+  
+  mm_empty_t *empties;
+  int empty_count;
+};
+
+int        mm_type(const char *word);
+mm_input_t mm_load(const char *path);
+mm_word_t  mm_peek(mm_input_t *input);
+mm_word_t  mm_take(mm_input_t *input);
+int        mm_expect(mm_input_t *input, int type, mm_word_t *save);
+
+void mm_parse(mm_input_t *input, mm_output_t *output);
+
+// stage 0: generate proper code, creating labels and references as found, and filling with 0 all references
+// stage 1: go back to all references, parsing simple expressions and replacing the 0s with the results
 
 #endif
---
+++ mmasm.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <mmasm.h>
+#include <stdio.h>
+
+int mm_type(const char *word) {
+  
+}
+
+mm_input_t mm_load(const char *path) {
+  
+}
+
+mm_word_t mm_peek(mm_input_t *input) {
+  if (input->offset >= input->count) return (mm_word_t){.type = mm_word_eof};
+  return input->words[input->offset];
+}
+
+mm_word_t mm_take(mm_input_t *input) {
+  if (input->offset >= input->count) return (mm_word_t){.type = mm_word_eof};
+  return input->words[input->offset++];
+}
+
+int mm_expect(mm_input_t *input, int type, mm_word_t *save) {
+  mm_word_t word = mm_peek(input);
+  
+  if (word.type == type) {
+    if (word.type != mm_type_eof) input->offset++;
+    if (save) *save = word;
+    
+    return 1;
+  }
+  
+  return 0;
+}
+
+void mm_parse(mm_input_t *input, mm_output_t *output) {
+  
+}
